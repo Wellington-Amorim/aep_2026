cab9cd2dfff9211eb6bcc32a23d7f55bcbe3bbd4
Wellington Amorim <amorimwellington788@gmail.com>
2026-03-23 01:22:29 -0300

    feat: adiciona historicostatus

---
+++ src/main/java/org/example/model/HistoricoStatus.java
@@ -0,0 +1,35 @@
+package org.example.model;
+
+import java.time.LocalDateTime;
+
+public class HistoricoStatus {
+    private StatusSolicitacao status;
+    private LocalDateTime data;
+    private String responsavel;
+    private String comentario;
+
+    protected HistoricoStatus() {}
+
+    public HistoricoStatus(StatusSolicitacao status, LocalDateTime data, String responsavel, String comentario) {
+        this.status = status;
+        this.data = data;
+        this.responsavel = responsavel;
+        this.comentario = comentario;
+    }
+
+    public StatusSolicitacao getStatus() {
+        return status;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public String getResponsavel() {
+        return responsavel;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+}
